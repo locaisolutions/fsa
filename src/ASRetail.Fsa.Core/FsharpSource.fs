41bde962ad033d82df9e3fa8c5288229a6899d78
rahicks26 <rahicks26@live.com>
2022-12-21 08:38:07 -0600

    Check source is valid AST

--- src/ASRetail.Fsa.Core/FsharpSource.fs
+++ src/ASRetail.Fsa.Core/FsharpSource.fs
@@ -1,6 +1,7 @@
 namespace ASRetail.Fsa.Core
 
 open System
+open FSharp.Compiler.CodeAnalysis
 
 type AnalysisError =
     | EmptyOrNullSourceProvided
@@ -11,9 +12,37 @@ type AnalysisError =
 /// to be analyzed.
 type FSharpSource = | Text of string
 
+type FsaContext = { typeChecker : FSharpChecker }
+
+module FsaContext =
+    let create () =
+        let typeChecker = FSharpChecker.Create () 
+        { typeChecker = typeChecker }
+
+module Ast =
+    open FSharp.Compiler.Text
+
+    let fromText { typeChecker = tc } input =
+        let file = "/temp/Temp.fsx"
+        let input = SourceText.ofString input
+
+        let projOptions, diagnostics =
+            tc.GetProjectOptionsFromScript (file, input, assumeDotNetFramework = false)
+            |> Async.RunSynchronously
+
+        let parsingOptions, _errors = tc.GetParsingOptionsFromProjectOptions (projOptions)
+
+        let parseFileResults =
+            tc.ParseFile (file, input, parsingOptions) |> Async.RunSynchronously
+
+        parseFileResults.ParseTree
+
 module FSharpSource =
     let parse =
         function
         | str when str |> String.IsNullOrWhiteSpace -> AnalysisError.EmptyOrNullSourceProvided |> Error
-        | str when str.Contains("let") -> str |> FSharpSource.Text |> Ok
+        | str when str.Contains ("let") ->
+            let ctx = FsaContext.create ()
+            let _ = Ast.fromText ctx str
+            str |> FSharpSource.Text |> Ok
         | _ -> "Invalid F#" |> AnalysisError.InvalidSourceCode |> Error
